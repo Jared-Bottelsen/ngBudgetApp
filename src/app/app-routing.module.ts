b652a41162bf9e38ce164b9556a478820871ca44
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-10-03 22:26:08 -0600

    Add new routes to the routing module for overview and budget add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BudgetOverviewComponent } from './budget-overview/budget-overview.component';
+import { BudgetAddComponent } from './budget/budget-add/budget-add.component';
 import { BudgetComponent } from './budget/budget.component';
 
 const routes: Routes = [
   {path: 'budget', component: BudgetComponent},
-  {path: 'overview', component: BudgetOverviewComponent}
+  {path: 'overview', component: BudgetOverviewComponent},
+  {path: 'budget-add', component: BudgetAddComponent}
 ];
 
 @NgModule({
