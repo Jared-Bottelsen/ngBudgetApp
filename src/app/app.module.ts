28dc7bf3e9f02eda45a0c301933b75d467a9e2be
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-10-03 22:26:32 -0600

    App module changes for newly added components and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,41 @@ import { MainHeaderComponent } from './main-header/main-header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MobileFooterComponent } from './mobile-footer/mobile-footer.component';
 import { BudgetOverviewComponent } from './budget-overview/budget-overview.component';
-import {ScrollPanelModule} from 'primeng/scrollpanel';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { ProgressBarModule } from 'primeng/progressbar';
 import { BudgetComponent } from './budget/budget.component';
+import { ProgressBarComponent } from './budget/progress-bar/progress-bar.component';
+import { BudgetAddComponent } from './budget/budget-add/budget-add.component';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
 @NgModule({
   declarations: [
     AppComponent,
     MainHeaderComponent,
     MobileFooterComponent,
     BudgetOverviewComponent,
-    BudgetComponent
+    BudgetComponent,
+    ProgressBarComponent,
+    BudgetAddComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    ScrollPanelModule
+    ScrollPanelModule,
+    ProgressBarModule,
+    DynamicDialogModule,
+    BrowserAnimationsModule
+  ],
+
+  entryComponents: [
+    BudgetAddComponent
+  ],
+
+  exports: [
+    ProgressBarModule,
+    ProgressBarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
