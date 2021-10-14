e4df64a3481159432a2853404828df43b8865896
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-10-13 22:39:07 -0600

    Add expense add component to the router path of the new plus icon and change styling and markup of footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { BudgetOverviewComponent } from './budget-overview/budget-overview.component';
 import { BudgetAddComponent } from './budget/budget-add/budget-add.component';
 import { BudgetComponent } from './budget/budget.component';
+import { ExpenseAddComponent } from './budget/expense-add/expense-add.component';
 
 const routes: Routes = [
   {path: 'budget', component: BudgetComponent},
   {path: 'overview', component: BudgetOverviewComponent},
-  {path: 'budget-add', component: BudgetAddComponent}
+  {path: 'add-expense', component: ExpenseAddComponent}
 ];
 
 @NgModule({
