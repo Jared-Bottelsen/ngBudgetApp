fc4c827f7af3c39c623f01d6239eba0fe0d7e44d
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-10-19 17:31:08 -0600

    Created expense add component and added basic architecture. Implementation of the firebase connection exists in this code as well as far as this components communication with it.

---
+++ src/app/budget/expense-add/expense-add.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
+
+@Component({
+  selector: 'app-expense-add',
+  templateUrl: './expense-add.component.html',
+  styleUrls: ['./expense-add.component.scss']
+})
+export class ExpenseAddComponent implements OnInit {
+
+  selectedOption: string;
+  categoryOptions: any;
+  rawCateories: any = [];
+
+
+  expenseForm = this.fb.group({
+    expenseCategory: '',
+    expenseAmount: ''
+  })
+
+  optionValue(event: any) {
+    console.log(event);
+  }
+
+  formOptions = [
+    {
+      label: 'Expense',
+      value: 'expense'
+    },
+    {
+      label: 'Income',
+      value: 'income'
+    }
+  ]
+
+  createDate() {
+    let newDate = new Date();
+    let month = newDate.getMonth() + 1;
+    let day = newDate.getDate();
+    console.log(`${month}/${day}`);
+    return `${month}/${day}`
+  }
+
+  isolateOptions (options: any) {
+    let categories: any = [];
+    for (let i = 0; options.length > i; i++) {
+      let subCategories = options[i].subCategory
+      for (let j = 0; j < subCategories.length; j++) {
+        categories.push(subCategories[j].subCategoryTitle)       
+      }
+    }
+    return categories
+  }
+
+  onFormSubmit(formData: FormGroup) {
+    let categoryData = formData.value.expenseCategory.pop()
+    formData.value.expenseCategory = categoryData;
+    console.log(formData.value);
+    this.db.addExpense({expenseCategory: formData.value.expenseCategory,
+    expenseAmount: formData.value.expenseAmount, expenseDate: this.createDate()});
+  }
+
+  constructor(private fb: FormBuilder, private db: FirebaseService) { 
+    this.selectedOption = 'expense'
+    this.categoryOptions = [];
+  }
+
+  ngOnInit(): void {
+    // let ref = this.db.getCategories();
+    // ref.subscribe((categories) => {
+    //   this.categoryOptions = this.isolateOptions(categories);
+    //   console.log(this.categoryOptions);
+    // })
+    this.db.getCategories()
+    .subscribe((categories) => {
+      categories.map((data) => {
+        this.rawCateories.push(data.payload.doc.data())
+      });
+      this.categoryOptions = this.isolateOptions(this.rawCateories);
+    })
+  }
+}
