53faa7edaf67246b05d2f042c9738cc7e6856aae
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-10-23 19:57:29 -0600

    Add logic to connect expenses logged with the firebase service to run a query to fetch data and manipulate it to reflect the new value for the category after the expense has been subtracted. This was HARD AS HELL TO DO BUT IT WORKS (Though quite convoluted)

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/compat/firestore";
+import * as _ from 'lodash'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
- 
+ subCategoryCopy: any;
+ currentDocId!: string;
+ expenseValue!: string;
+ currentCategoryValue: any;
+ fullCurrentBudgetCategory: any;
+ queriedData: any;
+
   constructor(private database: AngularFirestore) {
   }
 
@@ -23,7 +30,41 @@ export class FirebaseService {
   }
 
   getCategories() {
-    return this.database.collection('budgetCategory').snapshotChanges();
+    return this.database.collection('budgetCategory').valueChanges();
 
+  }
+
+  getExpenseInfo(expenseValue: string, currentValue: string, fullCurrentBudgetCategory: any) {
+    this.expenseValue = expenseValue
+    this.currentCategoryValue = currentValue
+    this.fullCurrentBudgetCategory = fullCurrentBudgetCategory
+  }
+
+  private manipulateCategory(categoryCopy: any[], expenseValue: string, currentValue: any) {
+    for (let i = 0; i < categoryCopy.length; i++) {
+      if (_.isEqual(categoryCopy[i], this.fullCurrentBudgetCategory)) {
+        categoryCopy.splice(i, 1);
+        let expenseNum = parseInt(expenseValue);
+        let currentNum = parseInt(currentValue);
+        let newVal = currentNum - expenseNum;
+        let newValString = newVal.toString();
+        this.currentCategoryValue = newValString;
+        this.fullCurrentBudgetCategory.subCategoryValue = this.currentCategoryValue;
+        this.subCategoryCopy.push(this.fullCurrentBudgetCategory);
+        this.queriedData.subCategory = this.subCategoryCopy;
+      }     
+    }
+  }
+
+  expenseQuery(currentBudgetValue: any) {
+    let query = this.database.collection("budgetCategory", ref => ref.where("subCategory", "array-contains", currentBudgetValue)).get()
+    query.subscribe(results => {
+      results.forEach(test => {
+        this.queriedData = test.data();
+        this.subCategoryCopy = this.queriedData.subCategory;
+        this.manipulateCategory(this.subCategoryCopy, this.expenseValue, this.currentCategoryValue)
+        this.database.collection("budgetCategory").doc(test.id).update(this.queriedData);
+      })
+    })
   }
 }
