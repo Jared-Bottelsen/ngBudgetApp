6badd48afab99414073d5616d1be88f57c5a4484
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-11-02 17:28:32 -0600

    Revert Changes to environments. Ill tackle adding a dev vs prod build later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularFireDatabaseModule} from "@angular/fire/compat/database";
-// import { environment } from "../environments/environment";
-import { environmentProd } from '../environments/environment.prod';
+import { environment } from "../environments/environment";
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
 import { AppRoutingModule } from './app-routing.module';
@@ -42,8 +41,7 @@ import { InputTextModule } from 'primeng/inputtext'
     BudgetEditComponent
   ],
   imports: [
-    AngularFireModule.initializeApp(environmentProd.firebaseConfig),
-    // AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule,
     AngularFirestoreModule,
     BrowserModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,3 @@
-export const environmentProd = {
+export const environment = {
   production: true,
-
-  firebaseConfig : {
-    apiKey: "AIzaSyAV1r6TEFPQZ5-n3XJ-5PNKVkV38t4L4dc",
-    authDomain: "ngbudgetapp.firebaseapp.com",
-    databaseURL: "https://ngbudgetapp-default-rtdb.firebaseio.com",
-    projectId: "ngbudgetapp",
-    storageBucket: "ngbudgetapp.appspot.com",
-    messagingSenderId: "527651190506",
-    appId: "1:527651190506:web:1731715c9b36cb9f05c993",
-    measurementId: "G-0XD81FMWPP"
-  }
 };
