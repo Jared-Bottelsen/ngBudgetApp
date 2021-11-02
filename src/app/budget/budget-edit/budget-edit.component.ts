2626dec1981001ca2fa83d8b4540356639a390ba
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-11-02 09:45:32 -0600

    Implement CRUD operations within controller

--- src/app/budget/budget-edit/budget-edit.component.ts
+++ src/app/budget/budget-edit/budget-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog'; 
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-budget-edit',
@@ -13,7 +15,11 @@ export class BudgetEditComponent implements OnInit {
 
   editForm!: FormGroup;
 
-  constructor(private ref: DynamicDialogRef, private config: DynamicDialogConfig, private fb: FormBuilder) { 
+  isDeleteButtonVisibleCurrent: number = -1;
+
+  isDeleteButtonVisibleNew: number = -1;
+
+  constructor(private ref: DynamicDialogRef, private config: DynamicDialogConfig, private fb: FormBuilder, private db: FirebaseService) { 
     this.currentSubcategories = this.config.data.subCategories
     this.editForm = this.fb.group({
       categoryTitle: [this.config.data.categoryTitle],
@@ -37,7 +43,6 @@ export class BudgetEditComponent implements OnInit {
     this.currentSubCats.push(newCategory);
   }
 
-
   get newSubCats() {
     return this.editForm.get('newSubCategories') as FormArray
   }
@@ -52,14 +57,54 @@ export class BudgetEditComponent implements OnInit {
       subCategoryValue: [],
       startingValue: []
     })
-    this.newSubCats.push(newCategory)
+    this.newSubCats.push(newCategory);
+    this.isDeleteButtonVisibleCurrent = -1;
+    this.isDeleteButtonVisibleNew = -1;
+  }
+
+  deleteCurrentSubcategory(i: number) {
+    this.currentSubCats.removeAt(i);
+    this.isDeleteButtonVisibleCurrent = -1;
+    this.isDeleteButtonVisibleNew = -1;
+  }
+
+  deleteNewSubcategory(i: number) {
+    this.newSubCats.removeAt(i);
+    this.isDeleteButtonVisibleCurrent = -1;
+    this.isDeleteButtonVisibleNew = -1;
+  }
+
+  deleteEntireCategory() {
+    this.db.deleteDocument(this.currentSubcategories);
+    setTimeout(() => {
+      this.ref.close();
+    }, 500);
+  }
+
+  showDeleteButtonCurrent(i: number) {
+    this.isDeleteButtonVisibleNew = -1
+    this.isDeleteButtonVisibleCurrent = i;
+  }
+
+  showDeleteButtonNew(i: number) {
+    this.isDeleteButtonVisibleNew = i
+    this.isDeleteButtonVisibleCurrent = -1
   }
 
   onSubmit(payload: any) {
-    let newArray = [...payload.currentCategories, ...payload.newSubCategories]
-    console.log(newArray);
-    this.ref.close()
+    if (_.isEqual(payload.currentCategories, this.currentSubcategories) && this.config.data.categoryTitle === payload.categoryTitle && payload.newSubCategories.length === 0) {
+      this.ref.close();
+      console.log("No updates were made");
+    } else {
+    let newArray = [...payload.currentCategories, ...payload.newSubCategories];
+    let returnedObject = {
+      categoryTitle: payload.categoryTitle,
+      subCategory: newArray
+    }
+    this.db.updateBudgetCategoryData(this.currentSubcategories, returnedObject);
+    this.ref.close()  
   }
+}
 
   close() {
     this.ref.close();
