ff4d02da0f0985e87c7d8ddb624b95df4ab17fa3
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-11-02 17:31:48 -0600

    Add firebaseConfig to prod env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,12 @@ export const environment = {
   firebaseConfig : {
     apiKey: "AIzaSyAV1r6TEFPQZ5-n3XJ-5PNKVkV38t4L4dc",
     authDomain: "ngbudgetapp.firebaseapp.com",
+    databaseURL: "https://ngbudgetapp-default-rtdb.firebaseio.com",
     projectId: "ngbudgetapp",
     storageBucket: "ngbudgetapp.appspot.com",
     messagingSenderId: "527651190506",
-    appId: "1:527651190506:web:7fd047ac4cb484e205c993",
-    measurementId: "G-B4GH1EJ5JE"
+    appId: "1:527651190506:web:1731715c9b36cb9f05c993",
+    measurementId: "G-0XD81FMWPP"
   }
 };
 
