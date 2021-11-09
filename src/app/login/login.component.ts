5f868eb9224293471a345b250bb6e1e181a14fca
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-11-08 17:04:01 -0700

    Build out very basic login page component. Will add more detail and such to this in the future but for now it just holds the functionality of logging in with Google

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  openAuth() {
+    this.auth.googleLogin();
+  }
+
+  signOut() {
+    this.auth.signOut();
+  }
+
+}
