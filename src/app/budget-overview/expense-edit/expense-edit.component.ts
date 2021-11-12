8d0ca232f7ce8a7f796d14f4f3a9ab408dcc087b
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-11-11 20:26:02 -0700

    Implement basic edit and delete functionality for expenses. To come I will have to re-engineer the solution for updating the budget with the changed expense value (this is gonna suck)

--- src/app/budget-overview/expense-edit/expense-edit.component.ts
+++ src/app/budget-overview/expense-edit/expense-edit.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 interface ExpenseData {
-  expenseAmount: string,
+  expenseAmount: number,
   expenseCategory: string,
+  expenseName: string,
   expenseDate: string
 }
 
@@ -16,8 +19,31 @@ export class ExpenseEditComponent implements OnInit {
 
   expenseData!: ExpenseData;
 
-  constructor(private config: DynamicDialogConfig) {
+  expenseEditForm: FormGroup = this.fb.group({
+    expenseName: [this.config.data.expenseName],
+    expenseAmount: [this.config.data.expenseAmount],
+    expenseDate: [this.config.data.expenseDate]
+  })
+
+  constructor(private config: DynamicDialogConfig, private ref: DynamicDialogRef, private fb: FormBuilder, private db: FirebaseService) {
     this.expenseData = this.config.data
+  }
+
+  submitForm(formData: ExpenseData) {
+    let originalWithNewData: Object = {
+      original: this.config.data,
+      new: formData
+    }
+    this.ref.close(originalWithNewData)
+  }
+
+  closeModal() {
+    this.ref.close();
+  }
+
+  deleteExpense() {
+    this.db.deleteExpense(this.expenseData.expenseName, this.expenseData.expenseAmount, this.expenseData.expenseCategory);
+    this.ref.close()
   }
 
   ngOnInit(): void {
