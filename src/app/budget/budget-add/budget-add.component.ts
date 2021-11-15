7e616c0b0ba8749faa3f1a597ba3b137742ba348
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-11-15 15:29:19 -0700

    Style updates for budget-add component

--- src/app/budget/budget-add/budget-add.component.ts
+++ src/app/budget/budget-add/budget-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-budget-add',
@@ -9,10 +10,18 @@ import { DynamicDialogRef } from 'primeng/dynamicdialog';
 })
 export class BudgetAddComponent implements OnInit {
 
+  faTimes = faTimes;
+
+  indexOfSubCategoryDeleteButton: number = -1;
+
   budgetForm = this.fb.group({
     categoryTitle: '',
     subCategory: this.fb.array([]),
   });
+
+  constructor(private fb: FormBuilder, public ref: DynamicDialogRef) {}
+
+  ngOnInit(): void {}
 
   get subCategories() {
     return this.budgetForm.get('subCategory') as FormArray
@@ -23,12 +32,13 @@ export class BudgetAddComponent implements OnInit {
       subCategoryTitle: [],
       subCategoryValue: []
     })
-
     this.subCategories.push(subCategory);
+    this.indexOfSubCategoryDeleteButton = this.subCategories.length - 1
   }
 
   deleteSubCategory(i: any) {
     this.subCategories.removeAt(i);
+    this.hideDeleteButton();
   }
 
   onSubmit(budgetForm: FormGroup) {
@@ -37,10 +47,19 @@ export class BudgetAddComponent implements OnInit {
     } else {
       this.ref.close(budgetForm.value);
     }
+    this.hideDeleteButton();
   }
 
-  constructor(private fb: FormBuilder, public ref: DynamicDialogRef) {
+  closeModal() {
+    this.ref.close();
+    this.hideDeleteButton();
   }
-  ngOnInit(): void {
+
+  showDeleteButton(index: number) {
+    this.indexOfSubCategoryDeleteButton = index
+  }
+
+  hideDeleteButton() {
+    this.indexOfSubCategoryDeleteButton = -1;
   }
 }
