dab957823c27c40765b17b2dcd35293708b43db9
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2021-12-09 22:08:57 -0700

    Implement dynamic styling for progress bar component

--- src/app/budget/progress-bar/progress-bar.component.ts
+++ src/app/budget/progress-bar/progress-bar.component.ts
@@ -9,10 +9,27 @@ export class ProgressBarComponent implements OnInit {
 
 @Input('value') inputValue: number = 0;
 
+conditionGreen: boolean = false;
+
+conditionYellow: boolean = false;
+
+conditionRed: boolean = false;
+
   constructor() { 
   }
 
   ngOnInit(): void {
+    this.checkStatusValue();
+  }
+
+  checkStatusValue() {
+    if (this.inputValue <= 100 && this.inputValue > 50) {
+      this.conditionGreen = true;
+    } else if (this.inputValue <= 50 && this.inputValue > 25) {
+      this.conditionYellow = true 
+    } else {
+      this.conditionRed = true;
+    }
   }
 
 }
